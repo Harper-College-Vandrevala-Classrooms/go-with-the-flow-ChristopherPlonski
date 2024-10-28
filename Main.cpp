49f67e5a795a30cc9386a6443a6b3da895b59b50
Christopher Plonski <christopherplonski@gmail.com>
2024-10-28 15:07:42 -0500

    + more tests

--- Main.cpp
+++ Main.cpp
@@ -4,9 +4,8 @@
 
 using namespace std;
 
-int main() {
-	cout << "Starting main. \n";
-
+void test1() {
+	cout << "Test 1 \n";
 	map<int, double> sourcesAndSinks;
 	sourcesAndSinks[0] = 100.0;
 
@@ -18,6 +17,45 @@ int main() {
 
 	heatFlow.tick();
 	heatFlow.pretty_print();
+}
 
-	cout << "Main finished. \n";
+void test2() {
+	cout << "Test 2 \n";
+	map<int, double> sourcesAndSinks;
+	sourcesAndSinks[2] = 200.0;
+
+	HeatFlow heatFlow = HeatFlow(20.0, 5, 0.1, sourcesAndSinks, 20.0);
+	heatFlow.pretty_print();
+
+	heatFlow.tick();
+	heatFlow.pretty_print();
+
+	heatFlow.tick();
+	heatFlow.pretty_print();
+}
+
+void test3() {
+	cout << "Test 3 \n";
+	map<int, double> sourcesAndSinks;
+	sourcesAndSinks[0] = -100;
+	sourcesAndSinks[4] = -100;
+
+	HeatFlow heatFlow = HeatFlow(0.0, 5, 0.1, sourcesAndSinks, 0.0);
+	heatFlow.pretty_print();
+
+	heatFlow.tick();
+	heatFlow.pretty_print();
+
+	heatFlow.tick();
+	heatFlow.pretty_print();
+}
+
+int main() {
+	cout << "--Starting main. \n";
+
+	test1();
+	test2();
+	test3();
+
+	cout << "--Main finished. \n";
 }
